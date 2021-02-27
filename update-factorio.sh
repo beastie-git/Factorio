520eec11bb93cedce6d0ee9b0bf13753a2a69580
beastie-git <58933857+beastie-git@users.noreply.github.com>
2021-02-27 20:49:43 +0100

    Update update-factorio.sh

--- update-factorio.sh
+++ update-factorio.sh
@@ -17,7 +17,7 @@
 
 USERNAME=""
 TOKEN=""
-FACTORIO_BINARY_PATH=""
+FACTORIO_BINARY_FILE="bin/x64/factorio"
 PACKAGE="core-linux_headless64"
 APIVERSION="2"
 SERVER_VERSION=""
@@ -78,7 +78,7 @@ do
         USERNAME=$2
         printf "Username : %s\n" "$2"
       else
-        printf "[update-factorio.sh] syntax error, please use update-factorio.sh --help for more details\n"
+        printf "\033[0;31m[update-factorio.sh]\033[0m syntax error, please use update-factorio.sh --help for more details\n"
         exit 1
       fi
       shift
@@ -89,7 +89,7 @@ do
         TOKEN=$2
         printf "Token : %s\n" "$2"
       else
-        printf "[update-factorio.sh] syntax error, please use update-factorio.sh --help for more details\n"
+        printf "\033[0;31m[update-factorio.sh]\033[0m syntax error, please use update-factorio.sh --help for more details\n"
         exit 1
       fi
       shift
@@ -100,7 +100,7 @@ do
         FACTORIO_BINARY_PATH=$2
         printf "Path to factorio binary : %s\n" "$2"
       else
-        printf "[update-factorio.sh] syntax error, please use update-factorio.sh --help for more details\n"
+        printf "\033[0;31m[update-factorio.sh]\033[0m syntax error, please use update-factorio.sh --help for more details\n"
         exit 1
       fi
       shift
@@ -109,59 +109,60 @@ do
   shift
 done
 
-if [[ -z $USERNAME ]] || [[ -z $TOKEN ]] || [[ -z $FACTORIO_BINARY_PATH ]]
+if [[ -z $USERNAME || -z $TOKEN || -z $FACTORIO_BINARY_FILE ]]
 then
-  printf "[update-factorio.sh] Args missing, please use update-factorio.sh --help for more details\n"
+  printf "\033[0;31m[update-factorio.sh]\033[0m Args missing, please use update-factorio.sh --help for more details\n"
   exit 1
 fi
 
 #Get server version :
-SERVER_VERSION=$($FACTORIO_BINARY_PATH --version | awk '/Version:/ {print $2}')
-printf "[update-factorio.sh] server version : %s\n" "$SERVER_VERSION"
+SERVER_VERSION=$($FACTORIO_BINARY_FILE --version | awk '/Version:/ {print $2}')
+printf "\033[0;33m[update-factorio.sh]\033[0m server version : %s\n" "$SERVER_VERSION"
 
 #Get latest stable version :
-LATEST_STABLE_VERSION=$(printf "$AVAIABLE_VERSIONS" | sed -e s/},{/\\n/g -e s/}]}//g -e s/\"//g | awk 'BEGIN{FS=":"} /stable/ {printf $2}')
-printf "[update-factorio.sh] Latest stable version : %s\n" "$LATEST_STABLE_VERSION"
+LATEST_STABLE_VERSION=$(printf "%s\n" "$AVAIABLE_VERSIONS" | sed -E -e 's/},\{/\n/g' -e 's/\"|}]}//g' | awk 'BEGIN{FS=":"} /stable/ {printf $2}')
+printf "\033[0;33m[update-factorio.sh]\033[0m Latest stable version : %s\n" "$LATEST_STABLE_VERSION"
 
 # If needed, fetch the patch and update
 if [[ $SERVER_VERSION == $LATEST_STABLE_VERSION ]]
 then
-  printf "[update-factorio.sh] Factorio is on the latest stable version, skipping\n"
+  printf "\033[0;33m[update-factorio.sh]\033[0m Factorio is on the latest stable version, skipping\n"
   exit 0
 else
-  printf "[update-factorio.sh] Factorio is not on the latest stable version, checking for update\n"
-  if [[ $(printf "$AVAIABLE_VERSIONS" | sed -e s/},{/\\n/g | grep \"from\"\:\"$SERVER_VERSION\",\"to\"\:\"$LATEST_STABLE_VERSION\") ]]
+  printf "\033[0;33m[update-factorio.sh]\033[0m Factorio is not on the latest stable version, checking for update\n"
+  
+  if [[ $(printf "%s" "$AVAIABLE_VERSIONS" | sed -e 's/},{/\n/g' -e 's/\"//g' | grep from\:$SERVER_VERSION,to\:$LATEST_STABLE_VERSION) ]]
   then
-    printf "[update-factorio.sh] Update avaiable, trying to get download patch link :\n"
+    printf "\033[0;33m[update-factorio.sh]\033[0m Update avaiable, trying to get download patch link :\n"
     DOWNLOAD_LINK=$(GET https://updater.factorio.com/get-download-link?username=$USERNAME\&token=$TOKEN\&package=$PACKAGE\&from=$SERVER_VERSION\&to=$LATEST_STABLE_VERSION\&apiversion=$APIVERSION | sed -e s/\\[\"//g -e s/\"]//g)
     if [[ $DOWNLOAD_LINK == https* ]]
     then
       mkdir -p updates
-      printf "\t%s\n" "$DOWNLOAD_LINK"
-      printf "[update-factorio.sh] Patch avaiable, downloading it\n"
+      printf "\033[0;33m[update-factorio.sh]\033[0m \t%s\n" "$DOWNLOAD_LINK"
+      printf "\033[0;33m[update-factorio.sh]\033[0m Patch avaiable, downloading it\n"
       wget -O updates/update-$SERVER_VERSION-$LATEST_STABLE_VERSION.zip $DOWNLOAD_LINK
       ERROR=$?
       if [[ $ERROR == "0" ]]
       then
-        printf "[update-factorio.sh] Applying patch...\n"
-        $FACTORIO_BINARY_PATH --apply-update updates/update-$SERVER_VERSION-$LATEST_STABLE_VERSION.zip
+        printf "\033[0;33m[update-factorio.sh]\033[0m Applying patch...\n"
+        $FACTORIO_BINARY_FILE --apply-update updates/update-$SERVER_VERSION-$LATEST_STABLE_VERSION.zip
         if [[ $? ]]
         then
-          printf "[update-factorio.sh] Patch applied sucessfull\n"
+          printf "\033[0;33m[update-factorio.sh]\033[0m Patch applied sucessfull\n"
           exit 0
         else
-          printf "[update-factorio.sh] Error when applying the patch\n"
+          printf "\033[0;31m[update-factorio.sh]\033[0m Error when applying the patch\n"
           exit 1
         fi
       else
-        printf "[update-factorio.sh] Wget download failled whith return %s\n" "$ERROR"
+        printf "\033[0;31m[update-factorio.sh]\033[0m Wget download failled whith return %s\n" "$ERROR"
       fi
     else
-      printf "[update-factorio.sh] Getting link failed, api response : %s\n" "$DOWNLOAD_LINK"
+      printf "\033[0;31m[update-factorio.sh]\033[0m Getting link failed, api response : %s\n" "$DOWNLOAD_LINK"
       exit 1
     fi
   else
-    printf "[update-factorio.sh] No patch avaiable, exiting\n"
+    printf "\033[0;31m[update-factorio.sh]\033[0m No patch avaiable, exiting\n"
     exit 1
   fi
 fi
